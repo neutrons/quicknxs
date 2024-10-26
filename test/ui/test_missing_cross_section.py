58061080f4cc344ca017c64c7f512ec11ee55487
Jose Borreguero <borreguero@gmail.com>
2024-10-25 20:01:59 -0400

    Enable reflectivity threshold assertion in test
    Re-enable the previously commented-out assertion that checks if all test values are below the reflectivity threshold. This ensures the test suite correctly verifies the reflectivity criteria.
    
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- test/ui/test_missing_cross_section.py
+++ test/ui/test_missing_cross_section.py
@@ -32,7 +32,7 @@ def test_missing_cross_section(qtbot):
     print("TEST_REFLECTIVITY_THRESHOLD_VALUE =", TEST_REFLECTIVITY_THRESHOLD_VALUE)
     for i, value in enumerate(test):
         print(f"test[{i}] = {value}")
-    # assert np.all(test <= TEST_REFLECTIVITY_THRESHOLD_VALUE)
+    assert np.all(test <= TEST_REFLECTIVITY_THRESHOLD_VALUE)
     # check the x vs TOF plot has changed
     intensity_on_on = np.sum(ui_utilities.data_from_plot2D(main_window.xtof_overview))
     assert intensity_on_on / intensity_off_on < TEST_REFLECTIVITY_THRESHOLD_VALUE
