306ab865393a4071ccf9b6269ca89248f5c0d79f
Kevin A. Tactac <tactacka@ornl.gov>
2025-01-09 13:41:28 -0500

    remove outdated build_tools dir

--- build_tools/travis_ci_build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-python setup.py install
-coverage run test/data_handling_test.py
-pylint --rcfile build_tools/pylint.rc -f parseable quicknxs/interfaces/data_handling quicknxs/interfaces/event_handlers
