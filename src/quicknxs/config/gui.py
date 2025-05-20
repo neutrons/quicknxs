85207f12690524606c87b0d35096262196bafaf9
Glass <glass.ships@outlook.com>
2025-05-20 15:36:06 -0400

    Update gui.py

--- src/quicknxs/config/gui.py
+++ src/quicknxs/config/gui.py
@@ -28,8 +28,8 @@ figure_params = [
 ]
 
 
-# QColors
 class QColors:
+    """Container for reused QColors"""
     black = QColor(0, 0, 0)
     dark_grey = QColor(200, 200, 200)
     light_grey = QColor(220, 220, 220)
