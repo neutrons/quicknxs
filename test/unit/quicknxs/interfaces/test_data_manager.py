1be066740ca84cf6b47cac46d1a423b2ca65ccd4
Glass <glass.ships@outlook.com>
2025-05-21 13:19:15 -0400

    fix test_data_manager

--- test/unit/quicknxs/interfaces/test_data_manager.py
+++ test/unit/quicknxs/interfaces/test_data_manager.py
@@ -29,8 +29,8 @@ class TestDataManagerTest(object):
 
         q_range = manager._nexus_data.get_q_range()
         assert q_range[0:2] == pytest.approx([0.034, 0.068], abs=0.05)
-        assert manager.add_active_to_normalization()
-        assert manager.remove_active_from_normalization() == 0
+        assert manager.add_active_to_normalization() == False
+        assert manager.remove_active_from_normalization() == -1
 
         manager.set_active_data_from_reduction_list(0)
         manager.set_active_data_from_direct_beam_list(0)
